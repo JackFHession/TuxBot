5bdd82ad194bc2cb1b26ebabfe5bdef9093f02f0
psauxgrep8 <jackfranklin899@gmail.com>
2024-06-11 23:37:20 +0100

    Automation

--- Server/Interfaces/discord_bot.py
+++ Server/Interfaces/discord_bot.py
@@ -79,7 +79,7 @@ class DiscordBot:
                                 existing_links = json.load(linkfile)
                                 for link in existing_links["links"]:
                                     print(link)
-                                    if str(linkid) == link.get("link_id"):
+                                    if link.get("link_id") in str(linkid):
                                         link["channels"].append(message.channel)
                                         print(link)
                                         break
