1fe948312051d8ce2fad7681594f7a2aa3ba35f7
psauxgrep8 <jackfranklin899@gmail.com>
2024-06-13 15:31:45 +0100

    Automation

--- Server/create_bot.py
+++ Server/create_bot.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-files_to_make = ["reminders.json", "role_mappings.json", "config.json", "discord_key.json", "WelcomeUsers.json", "JURISDICTION.json"]
+files_to_make = ["links.json", "reminders.json", "role_mappings.json", "config.json", "discord_key.json", "WelcomeUsers.json", "JURISDICTION.json"]
 
 for file in files_to_make:
     os.system(f"mkdir ./Settings && touch {file}")
